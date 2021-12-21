535b600a9ebea2e2b03267f61fb038b9ba3b14d8
ceedeno <c.calderon0493@gmail.com>
2021-12-21 13:05:09 -0600

    Refactor: todo.js created. Component separated from App

--- src/App.js
+++ src/App.js
@@ -3,35 +3,8 @@ import './App.css';
 import {Button, Card, Container} from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import ListGroup from "react-bootstrap/ListGroup";
-import {DragDropContext, Draggable, Droppable} from "react-beautiful-dnd";
-import Collapse from "react-bootstrap/Collapse";
-
-function Todo({onDone, title, description, id, index}) {
-    const [open, setOpen] = useState(false);
-
-    return (
-        <Draggable draggableId={id.toString()} index={index}>
-            {(provided) => (
-                <div
-                    {...provided.draggableProps}
-                    {...provided.dragHandleProps}
-                    ref={provided.innerRef}>
-                    <div className="todo-container" onClick={() => setOpen(!open)}>
-                        <div>
-                            {title}
-                        </div>
-                        <Button className={"done-btn"} variant="success" onClick={onDone}>Done</Button>
-                    </div>
-                    <Collapse in={open}>
-                        <div className="text-muted ">
-                            {description}
-                        </div>
-                    </Collapse>
-                </div>
-            )}
-        </Draggable>
-    )
-}
+import {DragDropContext, Droppable} from "react-beautiful-dnd";
+import Todo from "./components/todo";
 
 function NewTodo(props) {
     return (
