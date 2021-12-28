3f363b4839117a42411a48aa175f3730ba4ecaef
ceedeno <c.calderon0493@gmail.com>
2021-12-28 11:07:00 -0600

    demo added

--- src/constants/demo.js
+++ src/constants/demo.js
@@ -2,25 +2,34 @@ const listsDemo = [
     {
 
         id: "0.15296556682060003",
-        name: "list 1",
+        name: "John's birthday",
         todos: [
-            {title: '11', description: '111', id: '0.8813312168204948'},
-            {title: '12', description: '11', id: '0.024163335841638878'}
+            {title: 'buy red balloons', description: 'none', id: '0.8813312168204948'},
+            {title: 'grab some beers', description: 'none', id: '0.024163335841638878'},
+            {title: 'send invitation to his friends', description: 'none', id: '0.024163335841638124'}
         ]
 
     },
     {
         id: "0.07939087780684084",
-        name: "List 2",
+        name: "Groceries",
         todos: [
-            {title: '21', description: '21', id: '0.79615412358121'}
+            {title: 'Broccoli', description: 'Less than 1kg', id: '0.79615412358121'},
+            {title: 'Cereal ', description: 'none', id: '0.79615412358131'},
+            {title: 'Eggs', description: '18 pack', id: '0.79615412358141'},
+            {title: 'Butter', description: '1/2 lb', id: '0.79615412358151'},
+            {title: 'Yogurt', description: 'none', id: '0.79615412358161'},
+            {title: 'Orange Juice', description: 'at least 2 lts', id: '0.79615412358171'}
         ]
     },
     {
         id: "0.3119848446738622",
-        name: "List 3",
+        name: "Friday",
         todos: [
-            {title: '21', description: '21', id: '0.7820407507195746'}
+            {title: 'Pick up Groceries', description: 'none', id: '0.7820407507195746'},
+            {title: 'Book flight to Seattle', description: 'none', id: '0.7820407507195736'},
+            {title: 'Reserve table at Yam Yam', description: 'none', id: '0.7820407507195726'},
+            {title: 'register for half Marathon', description: 'none', id: '0.7820407507195716'}
         ]
     }
 ];
